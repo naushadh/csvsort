naushadh/csvsort

commit 792542d8d902cb59d557f4e96e3075c8e6c9c0c1

Reign in memory usage by refactoring hotspots found from profiling.
- fromCSV and toCSV revealed very high number of allocations and time share.
- removed `mkAsync` usage as it seems to only balloon memory usage with little performance benefit.
- although this fix is a good thing for space usage (6x better), we now have regressed performance (6x slower when --parallel=4, and 2x slower when --parallel=1). wip debugging this.

src/Lib.hs
@@ -147,9 +147,10 @@ createSortedSlices c hasHeader h dir
   |$ S.mapM (\rs -> VA.sort rs >> pure rs)
   |$ S.mapM V.unsafeThaw
   $ S.map V.fromList
-  . S.filter (not . null)
-  . fmap (fmap (uncurry Entity . validate pkIdx))
   -- |$ probe (putStrLn . (++) "fromCSV row#" . show . fst)
+  . S.mapM S.toList
+  . S.chunksOf 10000
+  . fmap (uncurry Entity . validate pkIdx)
   . fromCsv delim (configBufferSize c) rowId
   $ h
   where
@@ -206,11 +207,8 @@ mergeNFiles' c fs = do
   let dir = FP.takeDirectory fp1
   fpN <- getTempFile dir
   let delim = configDelimiter c
-  let flatten = S.concat :: S.Serial (S.Serial a) -> S.Serial a
   let fromCsv'
         = fmap (uncurry Entity . validate (configKeys c))
-        . flatten
-        . S.map S.fromList
         . fromCsv delim (configBufferSize c) 0
   let go hs = do
         let ss = fmap fromCsv' hs
@@ -232,8 +230,8 @@ mergeNFiles' c fs = do
 
 mergeNAsync :: Ord a => V.Vector (S.Serial a) -> S.Serial a
 mergeNAsync ss = do
-  ssA <- mapM (S.yieldM . S.mkAsync) ss
-  mhs <- mapM (S.yieldM . S.uncons) ssA
+  -- ssA <- mapM (S.yieldM . S.mkAsync) ss
+  mhs <- mapM (S.yieldM . S.uncons) ss
   let (hs,ssO) = V.unzip . fmap Maybe.fromJust . V.filter Maybe.isJust $ mhs
   let ihs = V.imap Indexed hs
   mergeN (Q.fromList . V.toList $ ihs) ssO
@@ -297,39 +295,37 @@ fromCsv
     , MonadIO (t m)
     , Csv.FromRecord a
     )
-  => Delim -> BufferSize -> Int -> IO.Handle -> t m [(RowId, Either String a)]
-fromCsv delim bSize initRowId h
-  = step (initRowId, Csvi.decodeWith opt Csv.NoHeader)
+  => Delim -> BufferSize -> Int -> IO.Handle -> t m (RowId, Either String a)
+fromCsv delim _bSize initRowId = S.unfoldrM (liftIO . go) . (,) initRowId
   where
     opt = Csv.DecodeOptions (fromIntegral . ord $ delim)
-    step (rowId, Csvi.Fail bs e)  = Exception.throw $ FileParseError rowId bs e
-    step (rowId, Csvi.Many rs k)  = consume rowId rs S..: next
-      where
-        next = do
-          p <- liftIO $ feed k
-          step (rowId+length rs, p)
-    step (rowId, Csvi.Done rs)    = S.yield (consume rowId rs)
-    consume rowId = zip [rowId..]
-    feed k = do
+    go (rowId, h) = do
+      let paramsNext = (rowId+1, h)
+      let mkRow x = return . pure $ ((rowId+1, x), paramsNext)
       isEof <- IO.hIsEOF h
       if isEof
-        then return $ k mempty
-        else k `fmap` BS.hGet h bSize
+        then pure Nothing
+        else do
+          l <- BS.hGetLine h
+          let res = Csv.decodeWith opt Csv.NoHeader . LBS.fromStrict $ l
+          case res of
+            Left err -> mkRow . Left $ err
+            Right rs -> if V.null rs
+                        then go (rowId+1, h)
+                        else mkRow . Right . V.head $ rs
 
 getPK :: PKIdx -> Row -> Maybe PK
 getPK pkIdx v = mapM ((V.!?) v) pkIdx
 
 {-# INLINABLE toCsv #-}
 toCsv
-  :: (S.MonadAsync m, Csv.ToRecord a)
+  :: ( Csv.ToRecord a)
   => Char
   -> FilePath
-  -> S.SerialT m a
-  -> m ()
-toCsv delim fp s
-  = (liftIO . LBS.writeFile fp)
-  . Csvi.encodeWith (csvEncodeOpt delim)
-  =<< mkBuilder
+  -> S.SerialT IO a
+  -> IO ()
+toCsv delim fp s = IO.withFile fp IO.WriteMode go
   where
-    appendRecord b x = b <> Csvi.encodeRecord x
-    mkBuilder = S.foldl' appendRecord mempty s
+    go h
+      = S.mapM_ (LBS.hPut h . Csv.encodeWith (csvEncodeOpt delim) . pure)
+      $ s

Makefile
@@ -2,13 +2,19 @@ define USAGE
 
 Available commands:
 
+  - setup: Setup development environment
+
 	- build: Compile all targets in pedantic mode with performance flags turned on.
+
+	- build-prof: Build all targets in pedantic mode with profiling flags turned on.
 
 	- seed: Seed a data file with 1 Million rows (customize using ROWS).
 
-	- run-base: Run `sort` and capture runtime stats.
+	- run-base: Run `sort` and capture basic runtime stats.
 
-	- run-x: Run `filesort` and capture runtime stats.
+	- run-x: Run `filesort` and capture basic runtime stats (changes args via ARGS).
+
+	- run-x-profile: Run `filesort` and capture detailed runtime stats (changes args via ARGS).
 
 	- test: A/B Test ./data file(s).
 
@@ -22,10 +28,18 @@ export USAGE
 TIMESTAMP := $(shell date +%Y-%m-%dT%H:%M:%S)
 MARKER ?= default
 ROWS ?= 1M
+ARGS ?= --keys "[0]" --in /tmp/in-${ROWS}.csv --output /tmp/out-x-${ROWS}.csv
+# ARGS ?= --keys "[0]" --in /tmp/in-${ROWS}.csv --output /tmp/out-x-${ROWS}.csv --parallel=1 --batch-size=16 --buffer-size=1M
 
 help:
 	@echo "$$USAGE"
 .PHONY: help
+
+setup:
+	mkdir -p .scratch
+	mkdir -p .scratch/debug .scratch/out .scratch/prof
+	stack exec which profiteur || stack install profiteur
+.PHONY: setup
 
 build:
 	stack install --verbosity warn --pedantic --ghc-options "-O2 -optc-O3 -optc-ffast-math -optc-march=core2 -fforce-recomp"
@@ -41,19 +55,26 @@ seed: build
 .PHONY: seed
 
 run-base:
-	(/usr/local/bin/time -v sort --key 1 /tmp/in-$(ROWS).csv --output /tmp/out-base-$(ROWS).csv) \
-	&> .scratch/out/base-$(ROWS)-$(TIMESTAMP)-$(MARKER).txt
+	(/usr/local/bin/time -v sort --key 1 /tmp/in-$(ROWS).csv --output /tmp/out-base-$(ROWS).csv --parallel=4 --batch-size=16 --buffer-size=10M) \
+	&> .scratch/out/base-$(TIMESTAMP)-$(MARKER)-$(ROWS).txt
 .PHONY: run-base
 
 run-x: build clean
-	(/usr/local/bin/time -v filesort --keys "[0]" --in /tmp/in-$(ROWS).csv --output /tmp/out-x-$(ROWS).csv) \
-	&> .scratch/out/x-$(ROWS)-$(TIMESTAMP)-$(MARKER).txt
+	(/usr/local/bin/time -v filesort ${ARGS} +RTS -s) \
+	&> .scratch/out/x-$(TIMESTAMP)-$(MARKER)-$(ROWS).txt
+
+	code .scratch/out/x-$(TIMESTAMP)-$(MARKER)-$(ROWS).txt
 .PHONY: run-x
 
-# run-x: build clean
-# 	(/usr/local/bin/time -v filesort --keys "[0]" --in /tmp/in-$(ROWS).csv --output /tmp/out-x-$(ROWS).csv --buffer-size=20M +RTS -p -sstderr) \
-# 	&> .scratch/out/x-$(ROWS)-$(TIMESTAMP)-Buff20MB.$(MARKER).txt
-# .PHONY: run-x
+run-x-prof: build-prof clean
+	(/usr/local/bin/time -v filesort ${ARGS} +RTS -p -sstderr) \
+	&> .scratch/out/x-$(TIMESTAMP)-prof-$(MARKER)-$(ROWS).txt
+
+	code .scratch/out/x-$(TIMESTAMP)-prof-$(MARKER)-$(ROWS).txt
+	mv filesort.prof .scratch/prof/filesort-$(TIMESTAMP)-$(MARKER)-$(ROWS).prof
+	stack exec -- profiteur .scratch/prof/filesort-$(TIMESTAMP)-$(MARKER)-$(ROWS).prof
+	open .scratch/prof/filesort-$(TIMESTAMP)-$(MARKER)-$(ROWS).prof.html
+.PHONY: run-x-prof
 
 test: build clean
 	sort --key 1,1 --output /tmp/test-base.csv data/comma-in-content.csv

stack.yaml
@@ -5,4 +5,4 @@ packages:
 
 extra-deps:
 - git: git@github.com:naushadh/streamly.git
-  commit: 8a5a8511a32fce43ff24785a5649fef2bdedf318
+  commit: d8daf99c7a3def70843120d57f51f8cef2e77ad4

commit a712ccf9bc5c4b5c3d7b792d2ffaaddcf6e8aa62

Minor performance improvements.
- Gains are more noticable in smaller file sizes (and ofcourse using just one capability).
- Finally traced down the cause (or rather the source code section) for poorer performance when using >1 capabilities. It appears it wasn't _just_ a regression introduced in the previous commit (792542d8d902cb59d557f4e96e3075c8e6c9c0c1), but something that always existed.

filesort.cabal
@@ -2,7 +2,7 @@
 --
 -- see: https://github.com/sol/hpack
 --
--- hash: 58636d41fa34a9a8a7c3b1fe2d24b6308324ee33ac07073dee129e7cb4edd572
+-- hash: a1352d8a45acc832ef61ca2bc03b19a86ce0da4528319e21e46679035eecf7ab
 
 name:           filesort
 version:        0.1.0.0
@@ -40,6 +40,7 @@ library
     , pqueue
     , streamly
     , temporary
+    , time
     , vector
     , vector-algorithms
     , vector-split
@@ -63,6 +64,7 @@ executable filesort
     , pqueue
     , streamly
     , temporary
+    , time
     , vector
     , vector-algorithms
     , vector-split
@@ -86,6 +88,7 @@ executable filesort-gen
     , pqueue
     , streamly
     , temporary
+    , time
     , vector
     , vector-algorithms
     , vector-split
@@ -109,6 +112,7 @@ test-suite filesort-test
     , pqueue
     , streamly
     , temporary
+    , time
     , vector
     , vector-algorithms
     , vector-split

package.yaml
@@ -31,8 +31,7 @@ dependencies:
 - directory
 - filepath
 - pqueue
-# - time
-# - text
+- time
 
 library:
   source-dirs: src

src/Lib.hs
@@ -26,6 +26,7 @@ import qualified Data.ByteString.Lazy as LBS
 import qualified Data.Vector as V
 import qualified Data.Vector.Split as Split
 import qualified Data.Vector.Algorithms.Merge as VA
+import qualified Data.Vector.Generic.Mutable as VGM
 import qualified Data.Csv as Csv
 import qualified Data.Csv.Incremental as Csvi
 import           Control.Monad.IO.Class (MonadIO, liftIO)
@@ -36,6 +37,8 @@ import qualified System.IO.Temp as Temp
 import qualified System.Directory as Dir
 import qualified System.FilePath as FP
 import qualified Data.PQueue.Min as Q
+
+import qualified Data.Time as Time
 
 -- logging
 -- import qualified Data.Time as Time
@@ -143,12 +146,8 @@ createSortedSlices c hasHeader h dir
   = S.runStream
   . S.asyncly
   $ S.mapM (saveTemp delim dir . fmap entityVal)
-  |$ S.mapM V.unsafeFreeze
-  |$ S.mapM (\rs -> VA.sort rs >> pure rs)
-  |$ S.mapM V.unsafeThaw
-  $ S.map V.fromList
-  -- |$ probe (putStrLn . (++) "fromCSV row#" . show . fst)
-  . S.mapM S.toList
+  |$ S.mapM sort'
+  |$ S.mapM S.toList
   . S.chunksOf 10000
   . fmap (uncurry Entity . validate pkIdx)
   . fromCsv delim (configBufferSize c) rowId
@@ -157,6 +156,11 @@ createSortedSlices c hasHeader h dir
     pkIdx = configKeys c
     delim = configDelimiter c
     rowId = startRowId hasHeader
+    -- internals are unsafe; trying to keep dangerous bits in one place.
+    sort' xs = do
+      mxs <- V.unsafeThaw . V.fromList $ xs
+      VA.sort mxs
+      V.unsafeFreeze mxs
 
 saveTemp :: Char -> FilePath -> V.Vector Row -> IO ()
 saveTemp delim dir rs = getTempFile dir >>= flip LBS.writeFile bs -- >> putStrLn "Saved to Temp"
@@ -194,11 +198,16 @@ mergeNFiles c fs
     n = V.length fs
     go = do
         let cfs = Split.chunksOf mf fs
+        -- begin time-sink zone; `mergeN` is slower when using >1 capabilities.
+        -- waiting on: https://github.com/composewell/streamly/issues/152
+        tStart <- Time.getCurrentTime
         S.runStream
           . S.asyncly
           . S.mapM (mergeNFiles' c)
           . S.fromList
           $ cfs
+        tEnd <- Time.getCurrentTime
+        print $ Time.diffUTCTime tEnd tStart
         pure . V.fromList $ fmap V.head cfs
 
 mergeNFiles' :: Config -> V.Vector FilePath -> IO FilePath
@@ -246,15 +255,17 @@ mergeN q ss
     go = case Q.minView q of
           Nothing -> S.nil
           Just (Indexed i x, qRem) -> do
-            case (ss V.!? i) of
-              Nothing -> pure x <> mergeN qRem ss
-              Just s -> do
-                mh <- S.yieldM . S.uncons $ s
-                let (qNext, oss)
-                      = case mh of
-                        Nothing -> (qRem, ss)
-                        Just (h, sNext) -> (Q.insert (Indexed i h) qRem, ss V.// [(i, sNext)])
-                pure x <> mergeN qNext oss
+            let s = ss V.! i
+            mh <- S.yieldM . S.uncons $ s
+            let (qNext, oss)
+                  = case mh of
+                    Nothing -> (qRem, ss)
+                    Just (h, sNext) -> (Q.insert (Indexed i h) qRem, modifyVectorIdx i sNext ss)
+            x `S.cons` mergeN qNext oss
+
+-- faster than `V.//` or `V.update`
+modifyVectorIdx :: Int -> a -> V.Vector a -> V.Vector a
+modifyVectorIdx i x = V.modify (\v -> VGM.write v i x)
 
 listDirectory :: FilePath -> IO [FilePath]
 listDirectory dir = fmap go <$> Dir.listDirectory dir